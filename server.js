27253d87cc01751e57ff1fd9d2f9382288c19d86
collins <collinsv406@gmail.com>
2026-04-14 17:38:22 +0100

    third commit

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const route = require('./route')
 const cors = require('cors')
 const express = require('express')
 const app = express()
-const PORT = 4000
+const PORT = process.env.PORT || 4000
 
 app.use(express.json());
 app.use(cors())
