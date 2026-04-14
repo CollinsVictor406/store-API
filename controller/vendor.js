675c52545b95d5251d8d00f5fbc34f8d3f413861
collins <collinsv406@gmail.com>
2026-04-14 19:05:10 +0100

    sixth commit

--- controller/vendor.js
+++ controller/vendor.js
@@ -3,14 +3,20 @@ const vendorModel = require('../model/vendorModel')
 exports.createInfo = async (req,res)=>{
   try {
    const { name,phoneNumber,businessType,currentPlatform,notes,otherBusinessType } = req.body
-    if(!name || !phone || !businessType || !notes){
+    if(!name || !phoneNumber || !businessType || !notes){
         return res.status(400).json({message:'Please fill in the necessary fields'})
     }
     if(businessType === 'Other' && !otherBusinessType){
      return res.status(400).json({message:'Please specify your business field'})
     }
-    const info = await vendorModel.create({ name,phoneNumber,businessType,currentPlatform,notes,otherBusinessType })
-    return res.status(201).json({info,message:'Info saved successfully'})
+    const userData = { name, phoneNumber,businessType,currentPlatform,notes,otherBusinessType}
+    if(businessType === 'Other'){
+      userData.otherBusinessType = otherBusinessType
+    }
+    const newUser = await vendorModel.create(userData)
+    console.log(newUser);
+    
+    return res.status(201).json({newUser,message:'Info saved successfully'})
   } catch (error) {
     return res.status(500).json({error:error.message})
   }
@@ -19,6 +25,8 @@ exports.createInfo = async (req,res)=>{
 exports.getAllUsers = async(req,res)=>{
   try {
     const users = await vendorModel.find().select('name phoneNumber businessType createdAt')
+    console.log();
+    
     if(users.length < 1){
       return res.status(401).json({message:"No user found"})
     }
